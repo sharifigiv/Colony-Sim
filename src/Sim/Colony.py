665f4187c6ea359a179b973c5998e0fbdb4b3c17
Miumen <sharifigiv5954@gmail.com>
2023-02-04 11:50:58 +0330

    Fixed Colony

--- src/Sim/Colony.py
+++ src/Sim/Colony.py
@@ -6,43 +6,47 @@ class Colony:
         self.color = color
 
         self.people = []
-        self.worl = world
+        self.world = world
 
     def add_people(self, Person):
         self.people.append(Person)
         self.world.new_people(Person)
 
-        Person.colony = self.name
+        Person.colony = self
 
-    def update():
+    def update(self):
         for person in self.people:
             direction = choice([1, 2, 3, 4])
             person.update()
 
             if direction == 1:
-                if self.world.check_place(self.x - 5, self.y):
+                if self.world.check_place(person.x - 5, person.y):
                     person.x -= 5
                     
                 else:
-                    person.fight(self.world.get_person(self.x - 5, self.y))
+                    if self.world.check_place(person.x - 5, person.y):
+                        person.fight(self.world.get_person(person.x - 5, person.y))
 
             if direction == 2:
-                if self.world.check_place(self.x + 5, self.y):
+                if self.world.check_place(person.x + 5, person.y):
                     person.x += 5
 
                 else:
-                    person.fight(self.world.get_person(self.x, self.y + 5))
+                    if self.world.check_place(person.x + 5, person.y):
+                        person.fight(self.world.get_person(person.x + 5, person.y))
 
             if direction == 3:
-                if self.world.check_place(self.x, self.y - 5):
+                if self.world.check_place(person.x, person.y - 5):
                     person.y -= 5
 
                 else:
-                    person.fight(self.world.get_person(self.x, self.y - 5))
+                    if self.world.check_place(person.x, person.y - 5):
+                        person.fight(self.world.get_person(person.x, person.y - 5))
 
             if direction == 4:
-                if self.world.check_place(self.x, self.y + 5):
+                if self.world.check_place(person.x, person.y + 5):
                     person.y += 5
 
                 else:
-                    person.fight(self.world.get_person(self.x, self.y + 5))
+                    if self.world.check_place(person.x, person.y + 5):
+                        person.fight(self.world.get_person(person.x, person.y + 5))
