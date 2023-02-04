6cddb82f87d1f921c77bcf32d53cd9ab5484ccbe
Miumen <sharifigiv5954@gmail.com>
2023-02-04 11:25:25 +0330

    Fixed checking place

--- src/Sim/World.py
+++ src/Sim/World.py
@@ -1,16 +1,26 @@
 class World:
-    def __init__(self):
+    def __init__(self, x, y, width, height):
         self.all_people = []
+
+        self.x = x
+        self.y = y
+
+        self.width = width
+        self.height = height
 
     def new_people(self, Person):
         self.all_people.append(Person)
         
     def check_place(self, x, y):
-        for p in self.all_people:
-            if p.x == x and p.y == y:
-                return False 
+        if x >= self.x and x <= self.width + self.x and y >= self.y and y <= self.height + self.y:
+            for p in self.all_people:
+                if p.x == x and p.y == y:
+                    return False 
 
-        return True
+            return True
+
+        else:
+            return False
 
     def get_person(self, x, y):
         for p in self.all_people:
