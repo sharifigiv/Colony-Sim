c2ad02d11a6e457aee20bc310de79d7fa64a4a88
Miumen <sharifigiv5954@gmail.com>
2023-02-04 11:16:24 +0330

    Base Colony Class

---
+++ src/Sim/Colony.py
@@ -0,0 +1,46 @@
+from random import choice
+
+class Colony:
+    def __init__(self, name, color, world):
+        self.name = name
+        self.color = color
+
+        self.people = []
+        self.worl = world
+
+    def add_people(self, Person):
+        self.people.append(Person)
+        Person.colony = self.name
+
+    def update():
+        for person in self.people:
+            direction = choice([1, 2, 3, 4])
+            person.update()
+
+            if direction == 1:
+                if self.world.check_place(self.x - 5, self.y):
+                    person.x -= 5
+                    
+                else:
+                    person.fight(self.worlf.get_person(self.x - 5, self.y))
+
+            if direction == 2:
+                if self.world.check_place(self.x + 5, self.y):
+                    person.x += 5
+
+                else:
+                    person.fight(self.worlf.get_person(self.x, self.y + 5))
+
+            if direction == 3:
+                if self.world.check_place(self.x, self.y - 5):
+                    person.y -= 5
+
+                else:
+                    person.fight(self.worlf.get_person(self.x, self.y - 5))
+
+            if direction == 4:
+                if self.world.check_place(self.x, self.y + 5):
+                    person.y += 5
+
+                else:
+                    person.fight(self.worlf.get_person(self.x, self.y + 5))
